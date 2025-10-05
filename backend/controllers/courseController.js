ca6b44f5b23157655c96a2cf3c2052a1c6815bd8
adarsh5828 <adarshsharma9696@gmail.com>
2025-10-05 15:01:47 +0530

    added query with keyword in course Controller

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,37 +1,37 @@
-import Course from '../models/courseModel.js';
+import Course from "../models/courseModel.js";
 
+export const getCourse = async (req, res) => {
+  const { keyword, category } = req.query;
+  console.log("keyword, category", keyword, category);
 
-export const getCourse = async (req,res) => {
+  // title filter
+  const titleFilter = keyword
+    ? { title: { $regex: keyword, $options: "i" } }
+    : {};
+  console.log("titleFilter", titleFilter);
 
-    try{
-        const courses = await Course.find({})
-        .populate('category', 'name')
-        .populate('instructor','name');
-        return res.status(200).json(courses);
+  try {
+    const courses = await Course.find({ ...titleFilter })
+      .populate("category", "name")
+      .populate("instructor", "name");
+    return res.status(200).json(courses);
+  } catch (error) {
+    return res.status(500).json({ message: "Server error!" });
+  }
+};
 
-    }catch(error){
+export const getCourseById = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id)
+      .populate("category", "name")
+      .populate("instructor", "name");
 
-        return res.status(500).json({message:'Server error!'})
+    if (!course) {
+      return res.status(404).json({ message: "COurse not found" });
     }
 
-}
-
-export const getCourseById = async (req,res) =>{
-    try{
-        const course  = await Course.findById(req.params.id)
-        .populate('category', 'name')
-        .populate('instructor','name');
-
-        if(!course){
-            return res.status(404).json({message:"COurse not found"});
-        }
-
-        return res.status(200).json(course);
-
-
-    }
-    catch(error){
-        return res.status(500).json({message:'Server error!'})
-
-    }
-} 
+    return res.status(200).json(course);
+  } catch (error) {
+    return res.status(500).json({ message: "Server error!" });
+  }
+};
